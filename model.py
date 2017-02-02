3faa4c33974774185ec5dbbb79ee2c566d46b37e
Cassandra Lamendola <cass@Athena.local>
2017-02-01 16:05:20 -0800

    Update Filter sizes

--- model.py
+++ model.py
@@ -24,8 +24,8 @@ for index, row in driving_log.iterrows():
 	y_train.append(row['STEERING_ANGLE'])
 X_train = np.array(X_train)
 y_train = np.array(y_train)
-print(X_train.shape)
-print(y_train.shape)
+#print(X_train.shape)
+#print(y_train.shape)
 
 ##### Split Data #####
 # Shuffle and separate training and test data
@@ -38,34 +38,35 @@ input_shape = X_train.shape[1:]
 pool_size = (2,2)
 strides = (1,1)
 
-n_filters1 = 6
-filter1 = 7
+n_filters1 = 3
+filter1 = 5
 
-n_filters2 = 12
-filter2 = 6
+n_filters2 = 6
+filter2 = 5
 
-n_filters3 = 24
+n_filters3 = 12
 filter3 = 5
 
-n_filters4 = 36
-filter4 = 4
+n_filters4 = 24
+filter4 = 3
 
-n_filters5 = 48
+n_filters5 = 36
 filter5 = 3
 
 keep_prob = 0.5
 output_shape1 = 100
 output_shape2 = 50
 output_shape3 = 10
+output_shape4 = 1
 
-batch_size = 32
-epochs = 10
+batch_size = 128
+epochs = 3
 learning_rate = 0.001
 
 ##### Create Model #####
 model = Sequential()
 
-# Convolutional Layer 1 (160 x 320 x 3) -> (164 x 314 x 6) -> (82 x 157 x 6)
+# Convolutional Layer 1 (160 x 320 x 3) -> (156 x 316 x 3) -> (78 x 158 x 3)
 model.add(Convolution2D(n_filters1, filter1, filter1, 
 	border_mode='valid',
 	subsample=strides, 
@@ -75,7 +76,7 @@ model.add(MaxPooling2D(pool_size=pool_size,
 	strides=strides, 
 	border_mode='valid'))
 
-# Convolutional Layer 2 (82 x 157 x 6) -> (77 x 152 x 12) -> (39 x 76 x 12)
+# Convolutional Layer 2 (78 x 158 x 3) -> (74 x 154 x 6) -> (37 x 77 x 6)
 model.add(Convolution2D(n_filters2, filter2, filter2, 
 	border_mode='valid',
 	subsample=strides))
@@ -84,7 +85,7 @@ model.add(MaxPooling2D(pool_size=pool_size,
 	strides=strides, 
 	border_mode='valid'))
 
-# Convolutional Layer 3 (39 x 76 x 12) -> (35 x 72 x 24) -> (18 x 36 x 24)
+# Convolutional Layer 3 (37 x 77 x 6) -> (33 x 73 x 12) -> (17 x 37 x 12)
 model.add(Convolution2D(n_filters3, filter3, filter3, 
 	border_mode='valid',
 	subsample=strides))
@@ -93,7 +94,7 @@ model.add(MaxPooling2D(pool_size=pool_size,
 	strides=strides, 
 	border_mode='valid'))
 
-# Convolutional Layer 4 (18 x 36 x 24) -> (15 x 33 x 36) -> (7 x 17 x 36)
+# Convolutional Layer 4 (17 x 37 x 12) -> (15 x 35 x 24) -> (8 x 18 x 24)
 model.add(Convolution2D(n_filters4, filter4, filter4, 
 	border_mode='valid',
 	subsample=strides))
@@ -102,7 +103,7 @@ model.add(MaxPooling2D(pool_size=pool_size,
 	strides=strides, 
 	border_mode='valid'))
 
-# Convolutional Layer 5 (7 x 17 x 36) -> (5 x 15 x 48) -> (3 x 8 x 48)
+# Convolutional Layer 5 (8 x 18 x 24) -> (6 x 16 x 36) -> (3 x 8 x 36)
 model.add(Convolution2D(n_filters5, filter5, filter5, 
 	border_mode='valid',
 	subsample=strides))
@@ -111,11 +112,11 @@ model.add(MaxPooling2D(pool_size=pool_size,
 	strides=strides, 
 	border_mode='valid'))
 
-# Flatten
+# Flatten 3 x 8 x 36 = 864
 model.add(Flatten())
 
 # Fully Connected Layer 1
-model.add(Dense(output_shape1, input_shape=(1152,)))
+model.add(Dense(output_shape1, input_shape=(864,)))
 model.add(Activation('relu'))
 model.add(Dropout(keep_prob))
 
@@ -126,12 +127,17 @@ model.add(Dropout(keep_prob))
 
 # Fully Connected Layer 3
 model.add(Dense(output_shape3))
+model.add(Activation('relu'))
+model.add(Dropout(keep_prob))
+
+# Output Layer
+model.add(Dense(output_shape4))
 
 ##### Train the model #####
 adam = Adam(lr=learning_rate)
 
-model.compile(optimizer=adam, 
-	loss='mean_squared_error', 
+model.compile(loss='mean_squared_error',
+	optimizer=adam,
 	metrics=['accuracy'])
 
 model.fit(X_train, y_train, 
